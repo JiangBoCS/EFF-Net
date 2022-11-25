1316ca2cfd010c3cd82ab4bf4ba5e73244aec6e1
JiangBoCS <73779399+JiangBoCS@users.noreply.github.com>
2022-11-25 18:37:19 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 python3 ./test_pad.py --arch EFF_Net --batch_size 1 --gpu '0' \
-    --input_dir /home/amax/Jiang_Test/Zheng_Shi_code/DN/Uformer/results/Input/ \
-    --gt_dir /home/amax/Jiang_Test/Zheng_Shi_code/DN/Uformer/results/Gt/ \
-    --result_dir /home/amax/Jiang_Test/Zheng_Shi_code/DN/Uformer/results/Ours_C/ \
-    --weights /home/amax/Jiang_Test/Zheng_Shi_code/DN/Uformer/log/EFF_NetEFF_N_G15/models/model_best.pth \
+    --input_dir /Input_G15/ \
+    --gt_dir /Gt/ \
+    --result_dir /Ours_CBSD68_G15/ \
+    --weights /G15_model_best.pth \
     --embed_dim 54
-    
-    
-#/home/amax/Jiang_Test/Zheng_Shi_code/DN/Uformer/log/Uformer_/models/
